80999da3c578174c51da458e2438d7c660fef54e
Wallace Batista <wallacebatistaoliveira@gmail.com>
2022-03-22 23:04:44 -0300

    chore: dependencies (#26)

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,19 +4,22 @@ module.exports = {
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@chakra-ui/storybook-addon",
   ],
   webpackFinal: async config => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": "@emotion/react",
-          "emotion-theming": "@emotion/react",
-        }
-      }
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@emotion/core": "@emotion/react",
+      "emotion-theming": "@emotion/react",
     }
+
+    return config
   }
 }
