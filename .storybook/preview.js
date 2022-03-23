cfe766c0a2d13547ef81787ddc1f75ce238d6026
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2022-03-22 22:59:34 -0300

    chore: dependencies

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,10 @@
-import { ChakraProvider, ThemeProvider } from '@chakra-ui/react'
 import { theme } from '../src/theme'
-
-export const decorators = [
-  (Story) => (
-    <ChakraProvider>
-      <ThemeProvider theme={theme}>
-        <Story />
-      </ThemeProvider>
-    </ChakraProvider>
-  )
-]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  chakra: {
+    theme,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
