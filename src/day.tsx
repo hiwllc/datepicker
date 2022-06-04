556da33bd35bff7b675dd35763960c8eaffcf5ce
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-06-04 11:24:20 +0200

    refactor: interface > type + use ButtonProps + spread styles

--- src/day.tsx
+++ src/day.tsx
@@ -1,27 +1,26 @@
 import * as React from 'react'
 import format from 'date-fns/format'
-import { Button, useStyleConfig } from '@chakra-ui/react'
+import { Button, ButtonProps, useStyleConfig } from '@chakra-ui/react'
 import { CalendarDate } from './types'
 
-export interface Day
-  extends React.PropsWithChildren<{
-    day: CalendarDate
-    variant?: 'selected' | 'range' | 'outside' | 'today'
-    disabled?: boolean
-    onSelectDate: (date: CalendarDate) => void
-  }> {}
+export type Day = React.PropsWithChildren<{
+  day: CalendarDate
+  variant?: 'selected' | 'range' | 'outside' | 'today'
+  onSelectDate: (date: CalendarDate) => void
+}> &
+  ButtonProps
 
-export function Day({ day, variant, disabled, onSelectDate, children }: Day) {
+export function Day({ day, variant, onSelectDate, children, ...props }: Day) {
   const styles = useStyleConfig('CalendarDay', { variant })
   // console.log({ variant, day })
 
   return (
     <Button
-      aria-label={format(day, 'MM-d')}
-      onClick={() => onSelectDate(day)}
-      sx={styles}
-      isDisabled={disabled}
       aria-current={variant === 'selected' ? 'date' : false}
+      aria-label={format(day, 'MM-d')}
+      {...props}
+      onClick={() => onSelectDate(day)}
+      sx={{ ...styles, ...props }}
     >
       {children || format(day, 'd')}
     </Button>
