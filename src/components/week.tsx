e9c6e1ab5df8f338c45eba227b921c89b5061810
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2022-07-16 01:22:59 -0300

    fix(week): rename getweek to getweekdays

--- src/components/week.tsx
+++ src/components/week.tsx
@@ -1,18 +1,18 @@
 import { Grid, Text, useMultiStyleConfig } from '@chakra-ui/react'
 import { addDays, format, startOfWeek } from 'date-fns'
 
-function getWeek() {
+function getWeekdays() {
   const start = startOfWeek(new Date())
   return [...Array(7).keys()].map(i => format(addDays(start, i), 'E'))
 }
 
 export function CalendarWeek() {
-  const week = getWeek()
+  const weekdays = getWeekdays()
   const styles = useMultiStyleConfig('CalendarMonth', {})
 
   return (
     <Grid sx={styles.week}>
-      {week.map(day => (
+      {weekdays.map(day => (
         <Text key={day} sx={styles.weekday}>
           {day}
         </Text>
